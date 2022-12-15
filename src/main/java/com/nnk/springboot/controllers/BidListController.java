fe427b77bab017bab6d33e1445ed949dca1491c4
Fercak Florian <fercak.florian@hotmail.fr>
2022-12-15 15:12:14 +0100

    debut implementation addBid

--- src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BidListController {
 	// TODO: Inject Bid service
-
+	
 	private BidListService bidListService;
 
 	public BidListController(BidListService bidListService) {

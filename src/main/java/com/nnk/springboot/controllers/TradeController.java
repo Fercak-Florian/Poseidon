e628965ae7d3578dc201b990591cb80a6bda53ce
Fercak Florian <fercak.florian@hotmail.fr>
2022-12-23 09:27:42 +0100

    première version du crud pour entité Trade

--- src/main/java/com/nnk/springboot/controllers/TradeController.java
+++ src/main/java/com/nnk/springboot/controllers/TradeController.java
@@ -1,11 +1,16 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.services.TradeService;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,28 +20,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TradeController {
     // TODO: Inject Trade service
+	
+	private TradeService tradeService;
+	
+	public TradeController(TradeService tradeService) {
+		this.tradeService = tradeService;
+	}
 
     @RequestMapping("/trade/list")
     public String home(Model model)
     {
         // TODO: find all Trade, add to model
+    	List<Trade> trades = tradeService.getTrades();
+    	model.addAttribute("trades", trades);	
         return "trade/list";
     }
 
     @GetMapping("/trade/add")
-    public String addUser(Trade bid) {
+    public String addUser(/*Trade bid*/) {
         return "trade/add";
     }
 
     @PostMapping("/trade/validate")
-    public String validate(@Validated Trade trade, BindingResult result, Model model) {
+    public String validate(@ModelAttribute @Validated Trade trade, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Trade list
+    	tradeService.saveTrade(trade);
         return "trade/add";
     }
 
     @GetMapping("/trade/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get Trade by Id and to model then show to the form
+    	Trade trade = tradeService.getTradeById(id).get();
+    	model.addAttribute("trade", trade);
         return "trade/update";
     }
 
@@ -44,12 +60,14 @@ public class TradeController {
     public String updateTrade(@PathVariable("id") Integer id, @Validated Trade trade,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Trade and return Trade list
-        return "redirect:/trade/list";
+    	tradeService.updateTrade(id, trade);
+    	return "redirect:/trade/list";
     }
 
     @GetMapping("/trade/delete/{id}")
     public String deleteTrade(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Trade by Id and delete the Trade, return to Trade list
+    	tradeService.deleteTrade(id);
         return "redirect:/trade/list";
     }
 }
---
+++ src/main/java/com/nnk/springboot/services/TradeService.java
@@ -0,0 +1,42 @@
+package com.nnk.springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.repositories.TradeRepository;
+
+@Service
+public class TradeService {
+
+	private TradeRepository tradeRepository;
+	
+	public TradeService(TradeRepository tradeRepository) {
+		this.tradeRepository = tradeRepository;
+	}
+	
+	public List<Trade> getTrades(){
+		return tradeRepository.findAll();
+	}
+	
+	public Optional<Trade> getTradeById(int id){
+		return tradeRepository.findById(id);
+	}
+	
+	public Trade saveTrade(Trade trade) {
+		return tradeRepository.save(trade);
+	}
+	
+	public Trade updateTrade(int id, Trade trade) {
+		Trade tradeToUpdate = tradeRepository.findById(id).get();
+		tradeToUpdate.setAccount(trade.getAccount());
+		tradeToUpdate.setType(trade.getType());
+		return tradeRepository.save(tradeToUpdate);
+	}
+	
+	public void deleteTrade(int id) {
+		tradeRepository.deleteById(id);
+	}
+}

65860c6f55e5f8f8453472271e34fce017eabeda
Fercak Florian <fercak.florian@hotmail.fr>
2022-12-26 11:29:31 +0100

    message erreur affiché à l'utilisateur lors d'une erreur de saisie lors de l'ajout d'un nouveau Bid

--- src/main/java/com/nnk/springboot/controllers/BidController.java
+++ src/main/java/com/nnk/springboot/controllers/BidController.java
@@ -2,23 +2,18 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Bid;
 import com.nnk.springboot.services.BidService;
-
 import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
 import java.util.Optional;
-
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*import javax.validation.Valid;*/
 
 @Slf4j
 @Controller
@@ -41,16 +36,19 @@ public class BidController {
 	}
 
 	@GetMapping("/bid/add")
-	public String addBidForm(/*BidList bid*/) {
+	public String addBidForm(Bid bid) {
 		log.info("display form to add bid");
 		return "bid/add";
 	}
 
 	@PostMapping("/bid/validate")
-	public String validate(@Validated Bid bid, BindingResult result, Model model) {
+	public String validate(@Valid Bid bid, BindingResult result, Model model) {
 		// TODO: check data valid and save to db, after saving return bid list
-		bidService.saveBid(bid);
-		log.info("successful bid adding");
+		if(!result.hasErrors()) {
+			bidService.saveBid(bid);
+			log.info("successful bid adding");
+			return "redirect:/bid/list";
+		}
 		return "bid/add";
 	}
 
@@ -66,7 +64,7 @@ public class BidController {
 	}
 
 	@PostMapping("/bid/update/{id}")
-	public String updateBid(@PathVariable("id") Integer id, @Validated Bid bid, BindingResult result,
+	public String updateBid(@PathVariable("id") Integer id, @Valid Bid bid, BindingResult result,
 			Model model) {
 		// TODO: check required fields, if valid call service to update Bid and return
 		// list Bid

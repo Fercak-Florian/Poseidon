4a0f0a2449de9cda25a9580857a298ad77820d60
Fercak Florian <fercak.florian@hotmail.fr>
2022-12-26 18:02:22 +0100

    ajout de messages d'erreurs lors de la saisie de valeurs incorrectes

--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,7 +1,7 @@
 package com.nnk.springboot.domain;
 
 import javax.persistence.*;
-/*import javax.validation.constraints.NotBlank;*/
+import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
 
@@ -31,21 +31,27 @@ public class RuleName {
 	@Column(name = "id")
 	private int id;
 	
+	@NotBlank(message = "Name is mandatory")
 	@Column(name = "name")
 	private String name;
 	
+	@NotBlank(message = "Description is mandatory")
 	@Column(name = "description")
 	private String description;
 	
+	@NotBlank(message = "Json is mandatory")
 	@Column(name = "json")
 	private String json;
 	
+	@NotBlank(message = "Template is mandatory")
 	@Column(name = "template")
 	private String template;
 	
+	@NotBlank(message = "sqlStr is mandatory")
 	@Column(name = "sql_str")
 	private String sqlStr;
 	
+	@NotBlank(message = "sqlPart is mandatory")
 	@Column(name = "sql_part")
 	private String sqlPart;
 }

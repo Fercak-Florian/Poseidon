1f073609cf7177a870ad2fdc637ea39057df5915
Fercak Florian <fercak.florian@hotmail.fr>
2023-01-09 12:04:55 +0100

    Javadoc écrite pour les classes RuleNameController et RuleNamService, ainsi que pour l'interface RuleNameRepository

--- src/main/java/com/nnk/springboot/services/RuleNameService.java
+++ src/main/java/com/nnk/springboot/services/RuleNameService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.repositories.RuleNameRepository;
 
+/**
+ * This class contains necessary methods to get, add, update and delete a RuleName
+ * This class uses the RuleNameRepository interface to perform this actions
+ */
 @Service
 public class RuleNameService {
 	
@@ -14,18 +18,41 @@ public class RuleNameService {
 		this.ruleNameRepository = ruleNameRepository;
 	}
 
+	/**
+     * This method finds all RuleName
+     * 
+     * @return a list of RuleName
+     */
 	public List<RuleName> getRuleNames(){
 		return ruleNameRepository.findAll();
 	}
 	
+	/**
+     * This method finds a RuleName by its id in database
+     * 
+     * @param a RuleName id
+     * @return a RuleName if it's found
+     */
 	public RuleName getRuleNameById(int id){
 		return ruleNameRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid ruleName Id : " + id));
 	}
 	
+	/**
+     * This method saves a RuleName
+     * 
+     * @param a RuleName to save
+     * @return the saved RuleName
+     */
 	public RuleName saveRuleName(RuleName ruleName) {
 		return ruleNameRepository.save(ruleName);
 	}
 	
+	/**
+     * This method finds and updates a RuleName
+     * 
+     * @param a RuleName to update and its id in database
+     * @return the updated RuleName
+     */
 	public RuleName updateRuleName(int id, RuleName ruleName) {
 		RuleName ruleNameToUpdate = ruleNameRepository.findById(id).get();
 		ruleNameToUpdate.setName(ruleName.getName());
@@ -37,6 +64,11 @@ public class RuleNameService {
 		return ruleNameRepository.save(ruleNameToUpdate);
 	}
 	
+	/**
+     * This method finds a RuleName and deletes it
+     * 
+     * @param a RuleName id in database
+     */
 	public void deleteRuleName(int id) {
 		RuleName ruleName = ruleNameRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid ruleName Id : " + id));
 		ruleNameRepository.delete(ruleName);

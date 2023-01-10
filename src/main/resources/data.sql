1135d971efbc6659a827517bd036789179263e08
Fercak Florian <fercak.florian@hotmail.fr>
2023-01-10 08:36:57 +0100

    page 403.html replacée dans le repertoire templates, utilisateur John Boyd supprimé de la base de donnée

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,5 @@
 
 insert into user(fullname, username, password, role) values("Administrator", "admin", "$2a$10$SfkQBJZn2dFINbyuDSkQHun2QvlPTpKqwC1sFMeih2/8o0.Kf4I2q", "ADMIN");
 insert into user(fullname, username, password, role) values("User", "user", "$2a$10$V41l6TiE50EGmzHIIqQS8Oab8ytmqTCAOM4iI2FhsiK5cgiK8wVF6", "USER");
-insert into user(fullname, username, password, role) values("John Boyd", "jboyd@email.com", "$2a$10$HolVU04ICQV1Ysdh6pFFv.yvsf5fmr5fop1koj6edbkgwIp21OB5a", "USER");
 
 /*Fin du script*/
